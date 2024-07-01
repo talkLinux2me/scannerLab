fc1ee57f608ac02b5305bdde32bea1ad9f65208f
talkLinux2me <167791277+talkLinux2me@users.noreply.github.com>
2024-07-01 12:27:37 -0400

    scanner Lab

---
+++ src/PartTwo.java
@@ -0,0 +1,22 @@
+
+        import java.util.Scanner;
+
+        public class PartTwo {
+
+            public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                int i = scan.nextInt();
+                double d = scan.nextDouble();
+                scan.nextLine();
+                String s = scan.nextLine();
+
+
+//                String s = "Welcome to HackerRank's Javscan.nextLine();a tutorials!";
+//                double d = 3.1415;
+//                int i = 42;
+
+                System.out.println("String: " + s);
+                System.out.println("Double: " + d);
+                System.out.println("Int: " + i);
+            }
+        }
